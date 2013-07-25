0b323b591c5482bd0f6b69a88c74623e6dd00f48
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2013-07-25 12:16:34 +0200

    avoid script generated cancel button

--- views/form_upload_progress_buttons.html.php
+++ views/form_upload_progress_buttons.html.php
@@ -1,6 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 
-<input type="submit" value="<?= t("Done")->for_html_attr() ?>" class="submit ui-state-default ui-corner-all" />
+<button type="submit" class="ui-state-default ui-corner-all">
+<?= t("Done") ?>
+</button>
 
 <button name="cancel" class="ui-state-default ui-corner-all" onclick="$('#h5up-input').h5up('cancel'); return false;" disabled="disabled">
 <?= t("Cancel uploads") ?>
