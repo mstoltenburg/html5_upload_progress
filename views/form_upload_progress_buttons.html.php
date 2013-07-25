db9bcf4a3f448daba34125aaa9a47ec4bbbc48ba
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2013-07-25 03:59:52 +0200

    buttons reloaded

--- views/form_upload_progress_buttons.html.php
+++ views/form_upload_progress_buttons.html.php
@@ -1,10 +1,9 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 
-<!-- Proxy the done request back to our form, since its been ajaxified -->
-<button id="g-upload-done" class="ui-state-default ui-corner-all" onclick="$(this.form).submit(); return false;">
-<?= t("Done") ?>
-</button>
-<button id="g-upload-cancel-all" class="g-cancel ui-state-default ui-corner-all" onclick="$('#h5up-input').h5up('cancel'); return false;">
+<input type="submit" value="<?= t("Done")->for_html_attr() ?>" class="submit ui-state-default ui-corner-all" />
+
+<button name="cancel" class="ui-state-default ui-corner-all" onclick="$('#h5up-input').h5up('cancel'); return false;" disabled="disabled">
 <?= t("Cancel uploads") ?>
 </button>
+
 <span id="g-add-photos-status-message" />
